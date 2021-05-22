2678c5cb8da2228e8dc68c37130b40ea895a4c15
Lucas Piputto <lucaspiputto@gmail.com>
2021-05-22 10:49:15 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// importamos express y mongoose
+const express  = require('express');
+const mongoose = require('mongoose');
+
+// importamos el router que creamos para la api
+const router = require('./routes/api/user');
+
+// puerto y base de datos
+const port = process.env.PORT        || 3000;
+const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
+
+const app = express();
+
+// conexion a la base de datos
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose
+    .connect(db, { useNewUrlParser : true })
+    .then(() => {
+        console.log(`DB connected @ ${db}`);
+    })
+    .catch(err => console.error(`Connection error ${err}`));
+
+// usamos el router
+app.use('/api', router);
+
+// el server escucha todo
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
